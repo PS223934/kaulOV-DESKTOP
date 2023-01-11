b0126da6e0e7df9a2a895a9c920c987cf58b2edc
ps231529 <ps231529@summacollege.nl>
2023-01-11 13:08:01 +0100

    desktop login base and connection string

---
+++ kaulOV/Models/KauloDB.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace kaulOV.Models
+{
+    class KauloDB
+    {
+        #region fields
+        MySqlConnection _connection = new MySqlConnection("Server = kaulov.mysql.database.azure.com; UserID = kaulovmain;Password=Ax74@3V&RUE2ohMnEfB8U^n78*NXzKve$yEtNnnApSIHg1rleB;Database=kaulov;SslMode=MySqlSslMode.Required;SslCa=ssl\\DigiCertGlobalRootCA.crt.pem;");
+        #endregion
+
+        #region methods/functions
+
+        #endregion
+    }
+}
---
+++ kaulOV/Views/LoginWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace kaulOV.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow(DataTable Politiek)
+        {
+            InitializeComponent(); 
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+
+    }
+}
