a0c935f79b22e2a4675e08aefc5c5455ed8092ad
ps231529 <ps231529@summacollege.nl>
2023-01-12 14:18:48 +0100

    Login + (proper) DB connection

---
+++ kaulOV/Classes/Auth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace kaulOV.Classes
+{
+    public class Auth
+    {
+        private readonly string _dbc = ConfigurationManager.ConnectionStrings["cString"].ConnectionString;
+
+        public bool Login(string email, string password)
+        {
+            using (MySqlConnection conn = new MySqlConnection(_dbc))
+            {
+                conn.Open();
+                string query = "SELECT * FROM users WHERE email = @Email AND password = @Password";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Password", password);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    MessageBox.Show("Login Successful");
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Login Failed");
+                    return false;
+                }
+
+            }
+        }
+    }
+}
--- kaulOV/Views/LoginWindow.xaml.cs
+++ kaulOV/Views/LoginWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,25 +9,50 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
+using kaulOV.Classes;
 
 namespace kaulOV.Views
 {
-    /// <summary>
-    /// Interaction logic for LoginWindow.xaml
-    /// </summary>
+    /// <summary>  
+        /// Interaction logic for MainWindow.xaml  
+        /// </summary>   
     public partial class LoginWindow : Window
     {
-        public LoginWindow(DataTable Politiek)
+        public LoginWindow()
         {
-            InitializeComponent(); 
+            InitializeComponent();
         }
-
-        private void Login_Click(object sender, RoutedEventArgs e)
+        Auth _Auth = new Auth();
+        Registration _Registration = new Registration();
+        Welcome _Welcome = new Welcome();
+        private void button1_Click(object sender, RoutedEventArgs e)
         {
-
+            if (textBoxEmail.Text.Length == 0)
+            {
+                errormessage.Text = "Enter an email.";
+                textBoxEmail.Focus();
+            }
+            else if (!Regex.IsMatch(textBoxEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                errormessage.Text = "Enter a valid email.";
+                textBoxEmail.Select(0, textBoxEmail.Text.Length);
+                textBoxEmail.Focus();
+            }
+            else
+            {
+                _Auth.Login(textBoxEmail.Text, passwordBox1.Password.ToString());
+            }
         }
-        
-
+        private void buttonRegister_Click(object sender, RoutedEventArgs e)
+        {
+            _Registration.Show();
+            Close();
+        }
     }
 }
